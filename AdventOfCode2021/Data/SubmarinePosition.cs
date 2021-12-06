bb6565f5a30d26101f4c232d914a75440db6a4b5
Matthew Pasker <mpasker@atlasrfid.com>
2021-12-06 09:00:49 -0600

    Remove commented out code.

--- AdventOfCode2021/Data/SubmarinePosition.cs
+++ AdventOfCode2021/Data/SubmarinePosition.cs
@@ -4,20 +4,8 @@ namespace AdventOfCode2021.Data
 {
     public class SubmarinePosition
     {
-        //public IDictionary<string, int> Position { get; set; }
         public int Distance { get; set; }
         public int Depth { get; set; }
         public int Aim { get; set; }
-
-        //public SubmarinePosition GetDefaultSubmarinePosition
-        //    => new()
-        //    {
-        //        Position = new Dictionary<string, int>
-        //        {
-        //            {"Distance", 0},
-        //            {"Depth", 0},
-        //            {"Aim", 0}
-        //        }
-        //    };
     }
 }
